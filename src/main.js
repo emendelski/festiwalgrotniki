9a32a8367b6d07556faab6a17aedfc0eabe8bdee
Kamil Mendelski <kamil@frontlabs.pl>
2019-06-25 09:21:08 +0200

    Moved static files to static/

--- src/main.js
+++ src/main.js
@@ -1,70 +1,37 @@
-import '../scss/style.scss';
+import './scss/style.scss';
 
-/* debouncedresize */
-function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100);}; return c;}
+function onReady() {
+  const $all = $('.choose-event');
+  const $events = $('.events-each');
 
-/* inspect userAgent - check if mobile, what browser, etc */
-var ua = window.navigator.userAgent.toLowerCase();
-var mobile = /mobile|android|kindle|silk|midp|phone|(windows .+arm|touch)/.test(ua);
+  $all.on('click', (e) => {
+    const that = e.currentTarget;
+    e.preventDefault();
 
-// http://www.zytrax.com/tech/web/browser_ids.htm
-// http://www.zytrax.com/tech/web/mobile_ids.html
-ua = /(chrome|firefox)[ \/]([\w.]+)/.exec(ua) || // Chrome & Firefox
-    /(iphone|ipad|ipod)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || // Mobile IOS
-    /(android)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || // Mobile Webkit
-    /(webkit|opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || // Safari & Opera
-    /(msie) ([\w.]+)/.exec(ua) ||
-    /(trident).+rv:(\w.)+/.exec(ua) || [];
+    const $target = $($(that).data('target'));
 
-var browser = ua[1];
-var version = parseFloat(ua[2]);
+    $all.not(that).addClass('inactive');
+    $(that).removeClass('inactive');
 
-var $body = $('body');
-var FGR = {
-      v: "v1.0.0",
-      DEBUG: true,
-      _: function(){
-        FGR.DEBUG && window.console && console.log.apply(console, arguments);
-      },
-      onReady: function(){
+    $events.removeClass('active');
+    $target.addClass('active');
 
-        FGR._("JS initiated. ", FGR.v, "\nBrowser informations:", browser, version);
+    $('document, html, body').animate({
+      scrollTop: $target.offset().top,
+    }, 1000);
+  });
 
-        var $all = $('.choose-event');
-        var $events = $('.events-each');
+  $('.js-close').on('click', (e) => {
+    e.preventDefault();
 
-        $all.on('click', function(e){
-          e.preventDefault();
+    $events.removeClass('active');
+    $all.addClass('inactive');
 
-          var $target = $($(this).data('target'));
-
-          $all.not(this).addClass('inactive');
-          $(this).removeClass('inactive');
-
-          $events.removeClass('active');
-          $target.addClass('active');
-
-          $('document, html, body').animate({
-            scrollTop: $target.offset().top
-          }, 1000);
-        });
-
-        $('.js-close').on('click', function(e){
-          e.preventDefault();
-
-          $events.removeClass('active');
-          $all.addClass('inactive');
-
-          $('document, html, body').animate({
-            scrollTop: 0
-          }, 1000);
-        });
-      }, // onReady
-      onResize: function(){
-
-      } // onResize
-    }; // FGR declarations
+    $('document, html, body').animate({
+      scrollTop: 0,
+    }, 1000);
+  });
+}
 
 // INIT
-on_resize(FGR.onResize)();
-$(document).ready(FGR.onReady);
+$(document).ready(onReady);
